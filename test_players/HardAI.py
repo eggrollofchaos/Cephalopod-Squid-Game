f8dbdf22d7e1130f439ca4c5fde46478837bcb3b
Wei Alexander Xin <eggrollofchaos@gmail.com>
2025-08-22 16:59:03 -0400

    “minor”

--- test_players/HardAI.py
+++ test_players/HardAI.py
@@ -65,7 +65,7 @@ class HardAI(BaseAI):
         
         return self.maximize_move(grid, alpha = -np.inf, beta = np.inf, depth = 0, start_time = start)
 
-    def maximize_move(self, grid: Grid, alpha : float, beta : float, depth : int, start_time):
+    def maximize_move(self, grid: Grid, alpha: float, beta : float, depth : int, start_time):
         """ 
         Description
         -----------
